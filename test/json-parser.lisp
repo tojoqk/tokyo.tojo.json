d48f49da32412193add9128b5a05579521c12dde
Masaya Tojo <masaya@tojo.tokyo>
2023-12-16 01:46:35 +0900

    Use local-nicknames to use tokyo.tojo.json-parser/json-parser

--- test/json-parser.lisp
+++ test/json-parser.lisp
@@ -1,8 +1,8 @@
 (defpackage #:tokyo.tojo.json-parser/test
-  (:use #:coalton-testing
-        #:tokyo.tojo.json-parser/json-parser)
+  (:use #:coalton-testing)
   (:local-nicknames
-   (#:map #:coalton-library/ord-map))
+   (#:map #:coalton-library/ord-map)
+   (#:json-parser #:tokyo.tojo.json-parser/json-parser))
   (:export #:run-tests))
 
 (in-package #:tokyo.tojo.json-parser/test)
@@ -19,96 +19,96 @@
    :interactive cl:t))
 
 (define-test parse-json-symbol-test ()
-  (matches (Ok JSON-Null)
-      (parse-json "null"))
-  (matches (Ok (JSON-Boolean True))
-      (parse-json "true"))
-  (matches (Ok (JSON-Boolean False))
-      (parse-json "false"))
+  (matches (Ok json-parser:JSON-Null)
+      (json-parser:parse-json "null"))
+  (matches (Ok (json-parser:JSON-Boolean True))
+      (json-parser:parse-json "true"))
+  (matches (Ok (json-parser:JSON-Boolean False))
+      (json-parser:parse-json "false"))
   (matches (Err _)
-      (parse-json "nil")))
+      (json-parser:parse-json "nil")))
 
 (define-test parse-json-number-test ()
-  (matches (Ok (JSON-Number (JSON-Integer 42)))
-      (parse-json "42"))
-  (matches (Ok (JSON-Number (JSON-Float 42.0d0)))
-      (parse-json "  42.0  "))
-  (matches (Ok (JSON-Number (JSON-Float 42.0d0)))
-      (parse-json "   42e0"))
-  (matches (Ok (JSON-Number (JSON-Float 420d0)))
-      (parse-json "42e001"))
-  (matches (Ok (JSON-Number (JSON-Float 4200d0)))
-      (parse-json "42e2"))
-  (matches (Ok (JSON-Number (JSON-Integer 123456789)))
-      (parse-json "123456789"))
-  (matches (Ok (JSON-Number (JSON-Float 4200d0)))
-      (parse-json "42e+2"))
-  (matches (Ok (JSON-Number (JSON-Float 0.42d0)))
-      (parse-json "42e-2"))
+  (matches (Ok (json-parser:JSON-Number (json-parser:JSON-Integer 42)))
+      (json-parser:parse-json "42"))
+  (matches (Ok (json-parser:JSON-Number (json-parser:JSON-Float 42.0d0)))
+      (json-parser:parse-json "  42.0  "))
+  (matches (Ok (json-parser:JSON-Number (json-parser:JSON-Float 42.0d0)))
+      (json-parser:parse-json "   42e0"))
+  (matches (Ok (json-parser:JSON-Number (json-parser:JSON-Float 420d0)))
+      (json-parser:parse-json "42e001"))
+  (matches (Ok (json-parser:JSON-Number (json-parser:JSON-Float 4200d0)))
+      (json-parser:parse-json "42e2"))
+  (matches (Ok (json-parser:JSON-Number (json-parser:JSON-Integer 123456789)))
+      (json-parser:parse-json "123456789"))
+  (matches (Ok (json-parser:JSON-Number (json-parser:JSON-Float 4200d0)))
+      (json-parser:parse-json "42e+2"))
+  (matches (Ok (json-parser:JSON-Number (json-parser:JSON-Float 0.42d0)))
+      (json-parser:parse-json "42e-2"))
   (matches (Err _)
-      (parse-json "0123"))
+      (json-parser:parse-json "0123"))
   (matches (Err _)
-      (parse-json "133."))
+      (json-parser:parse-json "133."))
   (matches (Err _)
-      (parse-json "133a")))
+      (json-parser:parse-json "133a")))
 
 (define-test parse-json-string-test ()
-  (matches (Ok (JSON-String "hello"))
-      (parse-json "\"hello\"")))
+  (matches (Ok (json-parser:JSON-String "hello"))
+      (json-parser:parse-json "\"hello\"")))
 
 (define-test parse-json-array-test ()
   (is (pipe "[\"hello\", 3, true, false  ]"
-            parse-json
-            (== (Ok (JSON-Array
-                     (make-list (JSON-String "hello")
-                                (JSON-Number (JSON-Integer 3))
-                                (JSON-Boolean True)
-                                (JSON-Boolean False)))))))
+            json-parser:parse-json
+            (== (Ok (json-parser:JSON-Array
+                     (make-list (json-parser:JSON-String "hello")
+                                (json-parser:JSON-Number (json-parser:JSON-Integer 3))
+                                (json-parser:JSON-Boolean True)
+                                (json-parser:JSON-Boolean False)))))))
   (matches (Err _)
-      (parse-json "[133, ]"))
+      (json-parser:parse-json "[133, ]"))
   (matches (Err _)
-      (parse-json "[133, , 3]"))
-  (is (pipe (parse-json "[133, [1, 2, 3], 3]")
-            (== (Ok (JSON-Array
-                     (make-list (JSON-Number (JSON-Integer 133))
-                                (JSON-Array
+      (json-parser:parse-json "[133, , 3]"))
+  (is (pipe (json-parser:parse-json "[133, [1, 2, 3], 3]")
+            (== (Ok (json-parser:JSON-Array
+                     (make-list (json-parser:JSON-Number (json-parser:JSON-Integer 133))
+                                (json-parser:JSON-Array
                                  (make-list
-                                  (JSON-Number (JSON-Integer 1))
-                                  (JSON-Number (JSON-Integer 2))
-                                  (JSON-Number (JSON-Integer 3))))
-                                (JSON-Number (JSON-Integer 3)))))))))
+                                  (json-parser:JSON-Number (json-parser:JSON-Integer 1))
+                                  (json-parser:JSON-Number (json-parser:JSON-Integer 2))
+                                  (json-parser:JSON-Number (json-parser:JSON-Integer 3))))
+                                (json-parser:JSON-Number (json-parser:JSON-Integer 3)))))))))
 
 (coalton-toplevel
   (define (make-object pairs)
-    (JSON-Object
+    (json-parser:JSON-Object
      (foldr (fn ((Tuple k v) m) (map:insert-or-replace m k v))
             map:empty
             pairs))))
 
 (define-test parse-json-object-test ()
   (is (pipe "{\"hello\" : \"world\" }"
-            parse-json
+            json-parser:parse-json
             (== (Ok (make-object
-                     (make-list (Tuple "hello" (JSON-String "world"))))))))
+                     (make-list (Tuple "hello" (json-parser:JSON-String "world"))))))))
   (is (pipe "
 {\"test\" : 42, \"object\": { \"true\": true, \"false\" : false },
  \"array\": [10, 3.2, \"test\"], \"null\" : null }"
-            parse-json
+            json-parser:parse-json
             (== (Ok (make-object
-                     (make-list (Tuple "test" (JSON-Number (JSON-Integer 42)))
+                     (make-list (Tuple "test" (json-parser:JSON-Number (json-parser:JSON-Integer 42)))
                                 (Tuple "object"
                                        (make-object
-                                        (make-list (Tuple "true" (JSON-Boolean True))
-                                                   (Tuple "false" (JSON-Boolean False)))))
+                                        (make-list (Tuple "true" (json-parser:JSON-Boolean True))
+                                                   (Tuple "false" (json-parser:JSON-Boolean False)))))
                                 (Tuple "array"
-                                       (JSON-Array
-                                        (make-list (JSON-Number (JSON-Integer 10))
-                                                   (JSON-Number (JSON-Float 3.2d0))
-                                                   (JSON-String "test"))))
-                                (Tuple "null" JSON-Null)))))))
-  (matches (Ok (JSON-Object map:empty))
-      (parse-json "{}"))
+                                       (json-parser:JSON-Array
+                                        (make-list (json-parser:JSON-Number (json-parser:JSON-Integer 10))
+                                                   (json-parser:JSON-Number (json-parser:JSON-Float 3.2d0))
+                                                   (json-parser:JSON-String "test"))))
+                                (Tuple "null" json-parser:JSON-Null)))))))
+  (matches (Ok (json-parser:JSON-Object map:empty))
+      (json-parser:parse-json "{}"))
   (matches (Err _)
-      (parse-json "{,}"))
+      (json-parser:parse-json "{,}"))
   (matches (Err _)
-      (parse-json "{\"test\" : 42,}")))
+      (json-parser:parse-json "{\"test\" : 42,}")))
